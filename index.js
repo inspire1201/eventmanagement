4fd94dc448d972ba416020869a8ecea7bbb83d4d
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-11 19:14:18 +0530

    sdjf

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const fs = require('fs');
 const cloudinary = require('cloudinary').v2;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'], 
+  credentials: true, 
+}));
 
 app.use(express.json());
 
