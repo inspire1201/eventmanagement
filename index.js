4757ea0e643eab3ba079d66a156c1b716dca1350
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-12 11:34:33 +0530

    f

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const cloudinary = require('cloudinary').v2;
 
 const app = express();
 app.use(cors({
-  origin: ['https://joyful-faun-e0c51a.netlify.app'], 
+  origin: ['https://joyful-faun-e0c51a.netlify.app','http://localhost:5173'], 
   credentials: true, 
 }));
 
-app.use(express.json());
+app.use(express.json());  
 
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
