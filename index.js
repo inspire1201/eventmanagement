7d1d56aa2d61bbd188d7f358be6ca38ffbd6884d
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-12 19:06:03 +0530

    s

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cloudinary = require('cloudinary').v2;
 
 const app = express();
 app.use(cors({
-  origin: ['https://incevenetmanagesystem.netlify.app','http://localhost:5173'], 
+  origin: ['https://silver-madeleine-58de6e.netlify.app','http://localhost:5173'], 
   credentials: true, 
 }));
 
