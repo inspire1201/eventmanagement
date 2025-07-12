9211865dee9fcfd90d5db518ba62d3769dccb813
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-12 13:25:53 +0530

    ashok

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cloudinary = require('cloudinary').v2;
 
 const app = express();
 app.use(cors({
-  origin: ['https://joyful-faun-e0c51a.netlify.app','http://localhost:5173'], 
+  origin: ['https://incevenetmanagesystem.netlify.app','http://localhost:5173'], 
   credentials: true, 
 }));
 
