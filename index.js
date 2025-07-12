4746a95e9827c1f0a17b515637e5a0340fd78336
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-12 11:16:59 +0530

    fd

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// Serve static files for media
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Multer setup for file uploads
