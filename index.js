da8f637293ec81d89fea793174b3b96c2f9d5915
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-16 14:36:52 +0530

    ahsh

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cloudinary = require('cloudinary').v2;
 
 const app = express();
 app.use(cors({
-  origin: ['https://incmanagement.netlify.app','http://localhost:5173'], 
+  origin: ['https://mellow-licorice-64a0d0.netlify.app','http://localhost:5173'], 
   credentials: true, 
 }));
 
