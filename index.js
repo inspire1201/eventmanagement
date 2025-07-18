c70544d23b384028b01325694def3ed32aea095c
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-18 23:28:51 +0530

    ashok

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cloudinary = require('cloudinary').v2;
 
 const app = express();
 app.use(cors({
-  origin: ['https://incevents.netlify.app','http://localhost:5173'], 
+  origin: ['https://luxury-bavarois-c37265.netlify.app','http://localhost:5173'], 
   credentials: true, 
 }));
 
