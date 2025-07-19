025e84a3763542748d44b36c02e3d915024d12cd
inspire1201 <inspiretransoft2002@gmail.com>
2025-07-19 19:35:36 +0530

    last work

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cloudinary = require('cloudinary').v2;
 
 const app = express();
 app.use(cors({
-  origin: ['https://incproject.netlify.app','http://localhost:5173'], 
+  origin: ['https://incmanagement.netlify.app','http://localhost:5173'], 
   credentials: true, 
 }));
 
